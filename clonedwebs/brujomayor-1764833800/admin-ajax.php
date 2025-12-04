a3868c63c4153acd72d41d7bc1fe244b2e607282
SaltBalente <saludablebela@gmail.com>
2025-12-04 02:37:32 -0500

    Add admin-ajax.php for brujomayor-1764833800

---
+++ clonedwebs/brujomayor-1764833800/admin-ajax.php
@@ -0,0 +1,20 @@
+<div class="wptwa-container wptwa-left-side" data-delay-time="0" data-inactive-time="0" data-scroll-length="0">
+					<div class="wptwa-box">
+						<div class="wptwa-description">
+							<p>Deseo tomar una consulta sin compromiso</p>
+
+						</div>
+						<span class="wptwa-close"></span>
+						<div class="wptwa-people">
+							<a href="https://api.whatsapp.com/send?phone=18645584008&text=Recibe una Consulta sin compromiso" data-number="18645584008" class="wptwa-account wptwa-clearfix wptwa-no-image" data-auto-text="Recibe una Consulta sin compromiso" data-ga-label="Brujo Mayor" target="_blank">
+								<div class="wptwa-face"><img src="" onerror="this.style.display='none'"></div>
+								<div class="wptwa-info">
+									<span class="wptwa-title">Consulta Gratis</span>
+									<span class="wptwa-name">Recibe Una Consulta</span>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="wptwa-toggle"><svg class="WhatsApp" width="20px" height="20px" viewBox="0 0 90 90"><use xlink:href="#wptwa-logo"></svg> <span class="wptwa-text">Consulta Sin Compromiso</span></div>
+					<div class="wptwa-mobile-close"><span>Close and go back to page</span></div>
+				</div>
